baacdb23f2bf14d2b3408845ed23dc6e9a541b39
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2019-02-13 22:42:28 +0100

    fixed the bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { StationComponent } from "./components/station/station.component";
-import { ChannelComponent } from "./components/channel/channel.component";
+import { ChannelComponent } from "./components/Channel/channel.component";
 import { SpinnerComponent } from "./components/spinner/spinner.component";
 import { ChannelDetailsComponent } from "./components/Channel/channel-details/channel-details.component";
 import { FooterComponent } from "./components/Channel/footer/footer.component";
